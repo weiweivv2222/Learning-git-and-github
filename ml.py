d6ce0adc09afcc8684f1c8e916601d764322d0dc
weiwei wang <wangweiweivv3w@gmail.com>
2020-12-03 04:06:53 +0100

    add 3 new import packages

--- ml.py
+++ ml.py
@@ -4,11 +4,11 @@ Python version: 3.7.3
 """
 
 #%%load packages for LDA
-<<<<<<< HEAD
-import numpy
-=======
-imoprt numpy as np
->>>>>>> 26be77cc833d7c2459371612c46407beb3da0752
+
+import numpy as np
+import panda as pd
+import system as sys
+
 
 #%% LDA
 def lda_pitchdecks(text_list, number_topics,number_words,counts=[0]):
