3232fa94f8b550da77e3cb506c7fed43b6d9d30d
weiwei wang <wangweiweivv3w@gmail.com>
2020-12-03 03:37:51 +0100

    add test file

---
+++ ml.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Python version: 3.7.3
+"""
+
+#%%load packages for LDA
+import os
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
+from datetime import date
+warnings.filterwarnings("ignore", category=FutureWarning)
+from sklearn.decomposition import LatentDirichletAllocation as LDA
+from sklearn.feature_extraction.text import CountVectorizer
+import pyLDAvis.sklearn
+
+import visualization 
+
+#%% LDA
+def lda_pitchdecks(text_list, number_topics,number_words,counts=[0]):
+    '''
+    output: the LDA of the text_list with the certain data preprocessing. 
+            counts[0] is the number of times this function is called as a index of the output file
+            file is saving in the C:\dev\doc\etap_platform
+    '''
+    # Initialise the count vectorizer
+    tf_vectorizer = CountVectorizer(strip_accents = 'unicode',
+                                stop_words = 'english',
+                                lowercase = True,
+                                token_pattern = r'\b[a-zA-Z]{3,}\b')
+    # generate word counts
+    dtm_tf = tf_vectorizer.fit_transform(text_list)
+  
+    # Create and fit the LDA model
+    lda = LDA(n_components=number_topics, random_state=0)
+    lda.fit(dtm_tf)
+    
+    # Print the topics found by the LDA model
+    print("Topics found via LDA:")
+    visualization.print_topics(lda, tf_vectorizer, number_words)
+    dtm_output=pyLDAvis.sklearn.prepare(lda, dtm_tf, tf_vectorizer)
+
+    outpath = r'C:\dev\doc\etap_platform' 
+    print('the output of the html file is in the following location',outpath)
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+    #run_date=date.today()
+    counts[0]+=1
+    file_path = os.path.join(outpath, 'LDA_{}.html'.format(counts[0]))
+    pyLDAvis.save_html(dtm_output,file_path)
